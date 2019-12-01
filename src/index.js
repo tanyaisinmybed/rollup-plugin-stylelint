955d0a83537a4de124ac7e1dfdc031731af53d05
Ivan Koleda <ikoleda@aristeksystems.com>
2019-12-02 00:22:03 +0300

    improve error formatting

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
-import { createFilter } from "rollup-pluginutils";
-import stylelint from "stylelint";
+const path = require('path');
+const pluginUtils = require("rollup-pluginutils");
+const stylelint = require("stylelint");
 
-export default function stylelintPlugin(options = {}) {
-    const filter = createFilter(options.include, options.exclude || "node_modules/**");
-
-    return {
-        name: "stylelint",
-        transform(code, id) {
-            if (!filter(id)) return;
-
-            return stylelint
-                .lint({...options, code, formatter: "verbose"})
-                .then(result => {
-                    if (result.errored) {
-                        throw Error(result.output);
-                    }
-                })
-                .catch(error => {
-                        throw Error(error);
-                });
-        }
-    };
+function resultHasErrors(result) {
+  return result.results.some(res => res.errored);
 }
+
+function normalizePath(id) {
+  return path
+    .relative(process.cwd(), id)
+    .split(path.sep)
+    .join("/");
+}
+
+function stylelintPlugin(options = {}) {
+  const filter = pluginUtils.createFilter(options.include, options.exclude || "node_modules/**");
+
+  return {
+    name: "stylelint",
+    transform(code, id) {
+      if (!filter(id)) return;
+      return stylelint
+        .lint({
+          code,
+          codeFilename: normalizePath(id),
+          formatter: "string",
+          ...options,
+        })
+        .then(result => {
+          if (result.output) {
+            process.stdout.write(result.output);
+            if (resultHasErrors(result)) {
+              throw new Error('Error(s) were found');
+            }
+          }
+        })
+        .catch(error => {
+          throw Error(error);
+        });
+    }
+  };
+}
+
+module.exports = stylelintPlugin;
